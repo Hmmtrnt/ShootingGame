eaa2d2ec9d2eb0f4114cdd21ab6dd0b4f79163a7
Hmmtrnt <rento5310@gmail.com>
2022-10-14 10:05:34 +0900

    フェードアウト処理追加

--- SceneTitle.cpp
+++ SceneTitle.cpp
@@ -4,18 +4,46 @@
 #include "SceneMain.h"
 #include "SceneResult.h"
 
+SceneTitle::SceneTitle()
+{
+	m_isEnd = false;
+	m_fadeBright = 0;
+	m_fadeSpeed = 0;
+}
+
 void SceneTitle::init()
 {
 	m_isEnd = false;
+	m_fadeBright = 0;
+	m_fadeSpeed = 8;
+}
+
+void SceneTitle::end()
+{
+	SetDrawBright(255, 255, 255);
 }
 
 SceneBase* SceneTitle::update()
 {
-	int padState = GetJoypadInputState(DX_INPUT_KEY_PAD1);
-	if (padState & PAD_INPUT_4)
+	m_fadeBright += m_fadeSpeed;
+	if (m_fadeBright >= 255)
 	{
-		// Mainに切り替え
+		m_fadeBright = 255;
+		m_fadeSpeed = 0;
+	}
+	if ((m_fadeBright <= 0) && (m_fadeSpeed < 0))
+	{
+		m_fadeBright = 0;
 		return(new SceneMain);
+	}
+
+	int padState = GetJoypadInputState(DX_INPUT_KEY_PAD1);
+	if (m_fadeSpeed == 0)
+	{
+		if (padState & PAD_INPUT_1)
+		{
+			m_fadeSpeed = -8;
+		}
 	}
 	
 	return this;
@@ -23,5 +51,7 @@ SceneBase* SceneTitle::update()
 
 void SceneTitle::draw()
 {
+	SetDrawBright(m_fadeBright, m_fadeBright, m_fadeBright);
 	DrawString(Game::kScreenWidth / 2 - 50, Game::kScreenHeight / 2 - 50, "やつを打て！", GetColor(255, 255, 255));
+	DrawString(Game::kScreenWidth / 2 - 75, Game::kScreenHeight / 2 + 20, "１ボタンでスタート", GetColor(255, 255, 255));
 }
--- SceneTitle.h
+++ SceneTitle.h
@@ -4,19 +4,22 @@
 class SceneTitle : public SceneBase
 {
 public:
-	SceneTitle()
-	{
-		m_isEnd = false;
-	}
+	SceneTitle();
+	
 	virtual ~SceneTitle() {}
-
+	// 初期化
 	virtual void init() override;
-	virtual void end() override {}
+	virtual void end() override;
 
 	virtual SceneBase* update() override;
+
 	virtual void draw() override;
 
 	virtual bool isEnd() { return m_isEnd; }
 private:
 	bool m_isEnd;
+	// フェード処理
+	int m_fadeBright;
+	// フェード速度
+	int m_fadeSpeed;
 };
